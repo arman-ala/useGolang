da986bd540c8588d6831225b22f2d1e299210245
arman-ala <arman.ala.developer@gmail.com>
2024-04-26 03:46:44 +0330

    Starting the use of go-chi in folder 5

--- 5/main.go
+++ 5/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	chi "github.com/go-chi/chi/v5"
 	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
 )
 
-func notFoundPage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintln(w, http.StatusText(http.StatusNotFound))
-}
-
-func homepage(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "<h1>Home Page</h1>")
@@ -29,25 +24,17 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Me!</h1><br/><p><a href=\"mailto:arman17gb@gmail.com\">arman17gb@gmail.com</a>")
 }
 
-type Router struct {}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-		case "/":
-			homepage(w, r)
-		case "/contact":
-			contactHandler(w, r)
-		case "/faq":
-			faqPage(w, r)
-        default:
-			notFoundPage(w, r)
-	}
-}
-
 func main() {
-	var router Router;
 	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqPage)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, http.StatusText(http.StatusNotFound))
+	})
 	fmt.Println(r)
 	fmt.Println("Starting the Server on port 3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
